0085d9c8f02e62d8f5a1dd7491b51af4849154d3
AlessioMantovani <alessiomantovani29@gmail.com>
2025-04-16 16:43:59 +0200

    Added plots submodule

--- jadaf/__init__.py
+++ jadaf/__init__.py
@@ -1,5 +1,6 @@
 from jadaf.core.jdf import JDF
 from jadaf.io.readers import load_csv, load_excel, load_json
+from jadaf.plots.plots import line_plot
 
-__all__ = ['JDF', 'load_csv', 'load_excel', 'load_json']
+__all__ = ['JDF', 'load_csv', 'load_excel', 'load_json', 'line_plot']
 __version__ = "0.1.0"
